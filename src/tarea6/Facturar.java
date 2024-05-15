5b4b1dbeaa9988cf79480cf84658cca82ad0b389
AlfredoValero <alfredo.valero@foc.es>
2024-05-15 21:05:02 +0200

    modificado comentario

--- src/tarea6/Facturar.java
+++ src/tarea6/Facturar.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tarea6;
 
 /**
  *
- * @author Fomento Ocupacional
+ * @author Buen examen a todos
  */
 public class Facturar {
     private static final int LIMITE_PARA_DESCUENTO = 3;
